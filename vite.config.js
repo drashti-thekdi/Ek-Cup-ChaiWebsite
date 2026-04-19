7fb79a23ecde8c8aecfbeefaf4a45fdc90e09d13
drashti-thekdi <85118452+drashti-thekdi@users.noreply.github.com>
2026-04-19 08:34:52 +0530

    Add base path for deployment in vite config

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: 
   [
     react(), 
+    
     tailwindcss(
       {
         theme: {
@@ -20,4 +21,6 @@ export default defineConfig({
         },
       },
     )],
+
+    base: '/Ek-Cup-ChaiWebsite/',
 })
